558c355d982150598c902ee815706cb9c1c5cb1e
Kapil Varshney <kvarshney.14@gmail.com>
2018-01-18 14:44:54 +0530

    Update exercise_0702.py

--- exercise_0702.py
+++ exercise_0702.py
@@ -4,6 +4,9 @@ X-DSPAM-Confidence:    0.8475
 Count these lines and extract the floating point values from each of the lines 
 and compute the average of those values and produce an output as shown below. 
 Do not use the sum() function or a variable named sum in your solution.
+
+Desired Output:
+Average spam confidence: 0.750718518519
 """
 
 
